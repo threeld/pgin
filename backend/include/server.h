96df73c294e1ac5a8bb6e21470eaf2b7148cdec6
gauravashisth <miya.musashi.moto1645@gmail.com>
2025-10-31 11:03:43 +0530

    Trigger CI: fix clang-format workflow

--- backend/include/server.h
+++ backend/include/server.h
@@ -4,9 +4,9 @@
 #include <arpa/inet.h>  // For inet_addr
 #include <signal.h>     // For signal handling
 #include <stdio.h>      // For printf
+#include <stdlib.h>     // For exit()
 #include <string.h>     // For memset, strlen
 #include <sys/socket.h> // For socket functions
 #include <unistd.h>     // For close()
-#include <stdlib.h>     // For exit()
 
 #endif /* SERVER_H */
